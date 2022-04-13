cae513f645280eda1cdb3a649b1cd29fd6b18e18
ajs124 <git@ajs124.de>
2022-04-13 13:02:35 +0100

    sampler: fix build with go 1.17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22079,9 +22079,7 @@ with pkgs;
 
   sambaFull = samba4Full;
 
-  sampler = callPackage ../applications/misc/sampler {
-    buildGoModule = buildGo116Module;
-  };
+  sampler = callPackage ../applications/misc/sampler { };
 
   shairplay = callPackage ../servers/shairplay { avahi = avahi-compat; };
 
